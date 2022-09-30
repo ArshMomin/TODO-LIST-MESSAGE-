67dc732b15ca15fea6d273f63bde2b93310bb5ad
Arsh Momin <arsh.momin2k@gmail.com>
2022-09-30 16:21:46 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import { collection, onSnapshot } from "firebase/firestore";
+// import { useEffect } from "react";
+import "./App.css";
+import Todo from "./components/Todo";
+// import { firestore } from "./firebaseConfig";
+
+function App() {
+  // useEffect(() => {
+  //   onSnapshot(collection(firestore, "todoList"), {
+  //     next: ({ docs }) => {
+  //       docs.forEach((doc) => {
+  //         console.log(doc.data(), doc.id);
+  //       });
+  //     },
+  //   });
+  //   console.log(firestore);
+  // }, []);
+
+  return <Todo />;
+}
+
+export default App;
